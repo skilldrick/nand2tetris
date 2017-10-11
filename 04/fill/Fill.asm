7241071fb01d4d79627fc76acdb169a74776d557
Nick Morgan <nickm@twitter.com>
2017-10-11 13:18:35 -0700

    Complete project 4

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,87 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+
+
+(LOOP)
+
+  // Load keyboard to D
+  @KBD
+  D=M
+
+  // Jump to WHITE if D is zero
+  @WHITE
+  D;JEQ
+
+(BLACK)
+  // screen length in words
+  @8192
+  D=A
+
+  // set R0 to screen length
+  @R0
+  M=D
+
+(BLACKLOOP)
+  @R0
+  M=M-1
+  D=M
+
+  // load R0 into D
+  @R0
+  D=M
+
+  // set A to R0 + SCREEN
+  @SCREEN
+  A=D+A
+
+  // make word black
+  M=-1
+
+  @R0
+  D=M
+  @BLACKLOOP
+  D;JNE
+
+
+  // jump to LOOP
+  @LOOP
+  0;JMP
+
+
+
+(WHITE)
+  // screen length in words
+  @8192
+  D=A
+
+  // set R0 to screen length
+  @R0
+  M=D
+
+(WHITELOOP)
+  @R0
+  M=M-1
+  D=M
+
+  // load R0 into D
+  @R0
+  D=M
+
+  // set A to R0 + SCREEN
+  @SCREEN
+  A=D+A
+
+  // make word white
+  M=0
+
+  @R0
+  D=M
+  @WHITELOOP
+  D;JNE
+
+  // jump to LOOP
+  @LOOP
+  0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,39 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+  // initialize R2
+  @R2
+  M=0
+
+
+(LOOP)
+  // check R0 is not zero
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+  // check R1 is not zero
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  // D is now R1. Add D to R2
+  @R2
+  M=D+M
+
+  // decrement R0
+  @R0
+  M=M-1
+
+  // load R0 into D
+  D=M
+
+  // jump back up
+  @LOOP
+  D;JMP
+
+(END)
+  @END
+  0;JMP
