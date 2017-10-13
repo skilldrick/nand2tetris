1d5b396ffa403f5fd56cdd437520ff225c5455f2
Nick Morgan <nickm@twitter.com>
2017-10-13 15:27:44 -0700

    Finish assembler

--- 06/assembler/index.js
+++ 06/assembler/index.js
@@ -11,12 +11,66 @@ const lines =
   }).filter(line => line.length);
 
 
+var currentMemoryLocation = 16;
+var labelCount = 0;
+const symbols = {
+  SP: 0,
+  LCL: 1,
+  ARG: 2,
+  THIS: 3,
+  THAT: 4,
+  R0: 0,
+  R1: 1,
+  R2: 2,
+  R3: 3,
+  R4: 4,
+  R5: 5,
+  R6: 6,
+  R7: 7,
+  R8: 8,
+  R9: 9,
+  R10: 10,
+  R11: 11,
+  R12: 12,
+  R13: 13,
+  R14: 14,
+  R15: 15,
+  SCREEN: 16384,
+  KBD: 24576
+};
+
+const linesWithoutLabels = lines.filter((line, index) => {
+  const match = line.match(/\(([a-zA-Z0-9_.$]*)\)/);
+
+  if (match) {
+    labelCount++;
+    symbols[match[1]] = index - labelCount + 1; // calculate line number of label
+    return false; // remove the label
+  } else {
+    return true;
+  }
+});
+
+
 function pad(num, length) {
   const missing0s = length - num.length;
   return (new Array(missing0s + 1)).join("0") + num;
 }
 
-function a(num) {
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function getSymbol(symbol) {
+  if (symbols[symbol] === undefined) {
+    symbols[symbol] = currentMemoryLocation++;
+  }
+
+  return symbols[symbol];
+}
+
+function a(numOrSymbol) {
+  const num = isNumeric(numOrSymbol) ? +numOrSymbol : +getSymbol(numOrSymbol);
   // A commands start with 0
   // Then convert num to binary padded to 15 bits
   return "0" + pad(num.toString(2), 15);
@@ -82,9 +136,9 @@ function c(line) {
   return "111" + comps[comp] + dests[dest] + jumps[jump];
 }
 
-const assembled = lines.map(line => {
+const assembled = linesWithoutLabels.map(line => {
   if (line[0] == '@') {
-    return a(+line.slice(1));
+    return a(line.slice(1));
   } else {
     return c(line);
   }
