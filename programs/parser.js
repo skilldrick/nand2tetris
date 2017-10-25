8cb8524829604f3167163d2946b553bef1859719
Nick Morgan <skilldrick@gmail.com>
2017-10-24 19:56:22 -0700

    Completed parser!

--- programs/parser.js
+++ programs/parser.js
@@ -277,7 +277,7 @@ function parse(tokens) {
       if (nextValueIsOneOf(binaryOperators)) {
         return [
           consumeOperator(),
-          consumeExpression()
+          consumeTerm()
         ];
       } else {
         return [];
